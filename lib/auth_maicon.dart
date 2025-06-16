b11dcca7f08c3b080e82daefc320154f87eb6f26
maiconkappauninfnet <maicon.kappaun@al.infnet.edu.br>
2025-06-16 19:36:26 -0300

    sigin

--- lib/auth_maicon.dart
+++ lib/auth_maicon.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-class PlanMarketAuth with ChangeNotifier {
+class AuthMaicon with ChangeNotifier {
 
   String? _message = null;
   String? _token = null;
