81c629c41d052dc2d4d3f7aa9b5dea290e63bce1
maiconkappauninfnet <maicon.kappaun@al.infnet.edu.br>
2025-06-16 19:26:53 -0300

    sigin

--- lib/auth_maicon.dart
+++ lib/auth_maicon.dart
@@ -1,17 +1,54 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-/// A Calculator.
-class AuthMaicon with ChangeNotifier {
-  String? message;
-  String? token;
+class PlanMarketAuth with ChangeNotifier {
+
+  String? _message = null;
+  String? _token = null;
 
   final _url = "https://identitytoolkit.googleapis.com";
   final _resource = "/v1/accounts";
   final _apiKey = "AIzaSyAHmqHNQ2dTfiY2SGdJhrwPm5FXc_V4Rm8";
 
-  void signUp() {}
+  Future<bool> signUp(String email, String password) async {
+    String sUri = '$_url$_resource:signUp?key=$_apiKey';
+    Uri uri = Uri.parse(sUri);
 
-  void signIn() {}
+    http.Response response = await http.post(uri, body: {
+        'email': email,
+        'password': password,
+        'returnSecureToken': 'true'
+    });
+
+    if (response.statusCode == 200) {
+      // outras operações
+      return true;
+    } else {
+      // outras operações
+      return false;
+    }
+  }
+
+  Future<bool> signIn(String email, String password) async {
+    String sUri = '$_url$_resource:signInWithPassword?key=$_apiKey';
+    Uri uri = Uri.parse(sUri);
+
+    http.Response response = await http.post(uri, body: {
+      'email': email,
+      'password': password,
+      'returnSecureToken': 'true'
+    });
+
+    if (response.statusCode == 200) {
+      // outras operações
+      return true;
+    } else {
+      // outras operações
+      return false;
+    }
+
+  }
+
 }
